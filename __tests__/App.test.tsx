e90e76733a0522418aa78b0bbc02c3c294011428
Vinicius <viniciuspsilvas@gmail.com>
2023-02-20 23:03:49 +1000

    jest-fetch-mock

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -1,10 +1,175 @@
-import { render, screen } from "@testing-library/react-native";
+import { act, fireEvent, render, screen } from "@testing-library/react-native";
 import * as React from "react";
 
 import App from "../App";
 
-test("renders correctly", () => {
-  render(<App />);
+import { useData, useZoneActions } from "../src/customHooks";
+import { Zone } from "../src/types";
 
-  expect(screen.getByTestId("app")).toBeOnTheScreen();
+jest.mock("../src/customHooks");
+
+global.fetch = jest.fn(() =>
+  Promise.resolve({
+    json: () =>
+      Promise.resolve({
+        data: {
+          controller: {
+            id: 1,
+            zones: [
+              {
+                id: 1,
+                name: "Zone 1",
+                icon: {
+                  id: 1,
+                  fileName: "leaf.png"
+                },
+                suspended: false,
+                status: {
+                  running: true
+                }
+              }
+            ]
+          }
+        }
+      })
+  })
+);
+
+test("shows loading when data are being fetched", () => {
+  useData.mockReturnValue({
+    loading: true,
+    data: {},
+    error: null
+  });
+
+  useZoneActions.mockReturnValue({
+    toggleZoneStatus: jest.fn((zone: Zone) => ({
+      id: 1
+    }))
+  });
+
+  render(<App />);
+  expect(screen.getByTestId("loading-spinner")).toBeOnTheScreen();
 });
+
+test("shows zones when data are fetched", async () => {
+  useData.mockReturnValue({
+    loading: false,
+    data: {
+      id: 1,
+      zones: [
+        {
+          id: 1,
+          name: "Zone 1",
+          icon: {
+            id: 1,
+            fileName: "leaf.png"
+          },
+          suspended: false,
+          status: {
+            running: true
+          }
+        }
+      ]
+    },
+    error: null
+  });
+
+  useZoneActions.mockReturnValue({
+    toggleZoneStatus: jest.fn((zone: Zone) => ({
+      id: 1
+    }))
+  });
+
+  render(<App />);
+  expect(screen.getByTestId("zoneName1")).toBeOnTheScreen();
+});
+
+test("shows the Data no found component", () => {
+  useData.mockReturnValue({
+    loading: false,
+    data: {
+      id: 1,
+      zones: []
+    },
+    error: null
+  });
+
+  useZoneActions.mockReturnValue({
+    toggleZoneStatus: jest.fn((zone: Zone) => ({
+      id: 1
+    }))
+  });
+
+  render(<App />);
+  expect(screen.getByTestId("dataNotFound")).toBeOnTheScreen();
+});
+
+function sleep(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+// test("renders correctly", () => {
+// const mData = {
+//   controller: {
+//     id: 1,
+//     zones: [
+//       {
+//         id: 1,
+//         name: "Zone 1",
+//         icon: {
+//           id: 1,
+//           fileName: "leaf.png"
+//         },
+//         suspended: false,
+//         status: {
+//           running: true
+//         }
+//       }
+//     ]
+//   }
+// };
+
+//   const mResponse = {
+//     ok: true,
+//     json: jest.fn().mockResolvedValue(mData)
+//   };
+//   global.fetch = jest.fn().mockResolvedValue(mResponse as unknown as Response);
+//   render(<App />);
+
+//   expect(screen.getByTestId("1ZoneDetail")).toBeOnTheScreen();
+// });
+
+// test("Press on the first zone", async () => {
+//   useData.mockReturnValue({
+//     loading: false,
+//     data: {
+//       id: 1,
+//       zones: [
+//         {
+//           id: 1,
+//           name: "Zone 1",
+//           icon: {
+//             id: 1,
+//             fileName: "leaf.png"
+//           },
+//           suspended: false,
+//           status: {
+//             running: true
+//           }
+//         }
+//       ]
+//     },
+//     error: null
+//   });
+
+//   render(<App />);
+//   expect(screen.getByTestId("iconPressable")).toBeOnTheScreen();
+//   expect(screen.getByText("Running...")).toBeOnTheScreen();
+
+//   act(() => {
+//     fireEvent.press(screen.getByTestId("iconPressable"));
+//   });
+
+//   expect(screen.queryByText("Running...")).toBeNull();
+// });
--- jest-setup.js
+++ jest-setup.js
@@ -4,9 +4,12 @@ import { configure } from '@testing-library/react-native';
 // Import Jest Native matchers
 import '@testing-library/jest-native/extend-expect';
 
+import { enableFetchMocks } from 'jest-fetch-mock'
+
 // Silence the warning: Animated: `useNativeDriver` is not supported because the native animated module is missing
 jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
 
+enableFetchMocks()
 // Enable excluding hidden elements from the queries by default
 configure({
   defaultIncludeHiddenElements: false,
